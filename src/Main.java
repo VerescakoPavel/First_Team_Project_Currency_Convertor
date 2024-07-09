89da0a081169d2f064c37fd56c0f37d4e5b9782c
Pavel Verescako <p.verescako@proton.me>
2024-07-09 19:37:34 +0200

    Pavel 1st commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            int inputChoice = 0;
+            System.out.println("Pls choose (number only) :");
+            System.out.println("1. Currency exchange");
+            System.out.println("2. View History");
+            System.out.println("3. Exit");
+
+                if (scanner.hasNextInt()) {
+                    inputChoice = scanner.nextInt();
+            } else {
+                    System.out.println("Wrong input, pls use only number");
+                    scanner.nextLine();
+                }
+
+        }
+
+
+
+
+
 
     }
 }
