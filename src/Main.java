f862cd05e7418a59abf2d9d3dc0d85a8d91cc6c8
Pavel Verescako <p.verescako@proton.me>
2024-07-09 19:42:13 +0200

    Pavel 1st commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
 
-
         Scanner scanner = new Scanner(System.in);
         while (true) {
             int inputChoice = 0;
@@ -13,19 +12,25 @@ public class Main {
             System.out.println("2. View History");
             System.out.println("3. Exit");
 
-                if (scanner.hasNextInt()) {
-                    inputChoice = scanner.nextInt();
+            if (scanner.hasNextInt()) {
+                inputChoice = scanner.nextInt();
             } else {
-                    System.out.println("Wrong input, pls use only number");
-                    scanner.nextLine();
+                System.out.println("Wrong input, pls use only number");
+                scanner.nextLine();
+
+                switch (inputChoice) {
+                    case (1):
+                        System.out.println("test");
+                        break;
+
+                    case (3):
+                        System.out.println("You choose exit");
+                        break;
                 }
 
+            }
+
+
         }
-
-
-
-
-
-
     }
 }
