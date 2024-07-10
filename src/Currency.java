9d700d3c2ce200e27450fc22ef4ba70a1bcc3477
IB <holt.irina@gmail.com>
2024-07-10 11:12:58 +0200

    Ira 3rd commit CurrencyClass fulfillment new (disregard previous 3rd commit)

--- src/Currency.java
+++ src/Currency.java
@@ -2,7 +2,10 @@ public class Currency {
     public String currencyName;
     public static final float USD_TO_EUR = 0.92f;
     public static final float USD_TO_GBP = 0.78f;
-    public static final float EUR_TO_GBP = 0.85f;
+    public static final float EUR_TO_GBP = 1 / (USD_TO_EUR / USD_TO_GBP);
+    public static final float EUR_TO_USD = 1 / USD_TO_EUR;
+    public static final float GBP_TO_USD = 1 / USD_TO_GBP;
+    public static final float GBP_TO_EUR = 1 / EUR_TO_GBP;
 
     public Currency(String currencyName) {
         this.currencyName = currencyName;
